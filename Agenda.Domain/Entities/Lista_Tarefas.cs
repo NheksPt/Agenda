9a39e63b2b95e46a06517502d316153df08a3f87
NheksPt <moriarty82@hotmail.com>
2024-01-30 00:47:39 +0000

    Add project files.

---
+++ Agenda.Domain/Entities/Lista_Tarefas.cs
@@ -0,0 +1,11 @@
+﻿namespace Agenda.Domain.Entities
+{
+    public class Lista_Tarefas
+    {
+        public int Id { get; set; }
+        public required string Tipo { get; set; }
+        public required string Descrição { get; set; }
+        public DateTime Criação { get; set; }
+    }
+   
+}
