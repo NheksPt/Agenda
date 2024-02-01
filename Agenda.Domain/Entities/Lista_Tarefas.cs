d8e8ae89145fee1c0b7fcb15cd0938ff01743bf6
NheksPt <moriarty82@hotmail.com>
2024-02-01 05:40:05 +0000

    Aula 10 completa

--- Agenda.Domain/Entities/Lista_Tarefas.cs
+++ Agenda.Domain/Entities/Lista_Tarefas.cs
@@ -5,6 +5,7 @@ namespace Agenda.Domain.Entities
     public class Lista_Tarefas
     {
         public int Id { get; set; }
+        [MaxLength(25)]
         public required string Tipo { get; set; }
         public required string Descrição { get; set; }
         public DateTime Criação { get; set; }
--- Agenda.Web/Controllers/Lista_TarefasController.cs
+++ Agenda.Web/Controllers/Lista_TarefasController.cs
@@ -27,12 +27,80 @@ namespace Agenda.Web.Controllers
         [HttpPost]
         public IActionResult Criar(Lista_Tarefas obj)
         {
-            if (ModelState.IsValid)
-            { 
-            _db.Listas_Tarefas.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+            if (obj.Tipo == obj.Descrição)
+            {
+                ModelState.AddModelError("Tipo", "Os campos Tipo e Descrição não podem ser iguais");
             }
+
+            if (ModelState.IsValid)
+            {
+                _db.Listas_Tarefas.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "A Lista foi criada com sucesso.";
+                return RedirectToAction("Index");
+            }
+
+            TempData["error"] = "Não foi possivel criar a Lista.";
+
+            return View(obj);
+        }
+
+        public IActionResult Editar(int lista_tarefasId)
+        {
+            Lista_Tarefas? obj = _db.Listas_Tarefas.FirstOrDefault(u => u.Id == lista_tarefasId);
+
+            if (obj is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(Lista_Tarefas obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _db.Listas_Tarefas.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "A Lista foi editada com sucesso.";
+                return RedirectToAction("Index");
+            }
+
+            TempData["error"] = "Não foi possivel editar a Lista.";
+
+            return View(obj);
+        }
+
+        public IActionResult Apagar(int lista_tarefasId)
+        {
+            Lista_Tarefas? obj = _db.Listas_Tarefas.FirstOrDefault(u => u.Id == lista_tarefasId);
+
+            if (obj is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Apagar(Lista_Tarefas obj)
+        {
+            Lista_Tarefas? objFromDb = _db.Listas_Tarefas.FirstOrDefault(u => u.Id == obj.Id);
+
+            if (objFromDb is not null)
+            {
+                _db.Listas_Tarefas.Remove(objFromDb);
+                _db.SaveChanges();
+                TempData["success"] = "A Lista foi apagada com sucesso.";
+                return RedirectToAction("Index");
+            }
+
+            TempData["error"] = "Não foi possivel apagar a Lista.";
+
             return View(obj);
         }
     }
