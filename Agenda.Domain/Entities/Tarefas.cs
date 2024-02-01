7e0bfc8036235260da49f4211d1b342cb0e1b877
NheksPt <moriarty82@hotmail.com>
2024-02-01 20:26:44 +0000

    aulas 11 ainda a funcionar antes de criar tabela

---
+++ Agenda.Domain/Entities/Tarefas.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Agenda.Domain.Entities
+{
+    public class Tarefas
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Tarefa { get; set; }
+
+        [ForeignKey("Lista_Tarefas")]
+        public int Lista_TarefasId { get; set; }
+        public Lista_Tarefas? Lista_Tarefas { get; set; }
+
+
+        public required string Nome { get; set; }
+        public required string Detalhes { get; set; } 
+        public enum Prioridade { Baixa, Média, Alta }
+        public DateTime Prazo { get; set; }
+
+    }
+}
